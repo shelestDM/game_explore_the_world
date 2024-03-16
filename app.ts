014581bc3d411b90349855c36dd37d9c397c40c4
bartashevich <shelest.dm@icloud.com>
2024-03-16 13:53:05 +0200

    second commit - player move and resource appear done

---
+++ app.ts
@@ -0,0 +1,121 @@
+let player_element: HTMLElement | null = document.getElementById("player");
+let resource_element: HTMLElement | null = document.getElementById("resource");
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+let player_size: Coordinate = { x: 50, y: 50 };
+let resource_size: Coordinate = { x: 20, y: 20 };
+
+function generateCoordinate(axis: number): number {
+  let coordinate: number = Math.floor(Math.random() * axis);
+  if (coordinate < 100) {
+    return coordinate + 100;
+  } else if (coordinate - resource_size.x) {
+    return coordinate - resource_size.x;
+  }
+  return coordinate;
+}
+
+let window_size: Coordinate = { x: window.innerWidth, y: window.innerHeight };
+let player_coordinate: Coordinate = { x: 0, y: 0 };
+
+let resource_coordinate: Coordinate = {
+  x: generateCoordinate(window_size.x),
+  y: generateCoordinate(window_size.y),
+};
+
+function setPositionByCoordinate(
+  x: number,
+  y: number,
+  element: HTMLElement | null
+): void {
+  if (element) {
+    element.style.top = y + "px";
+    element.style.left = x + "px";
+  }
+}
+
+setPositionByCoordinate(
+  player_coordinate.x,
+  player_coordinate.y,
+  player_element
+);
+setPositionByCoordinate(
+  resource_coordinate.x,
+  resource_coordinate.y,
+  resource_element
+);
+
+document.addEventListener("keydown", function (event): void {
+  // console.log(event.key);
+  switch (event.key.toLocaleLowerCase()) {
+    case "w":
+      player_coordinate.y -= 10;
+      break;
+    case "s":
+      player_coordinate.y += 10;
+      break;
+    case "a":
+      player_coordinate.x -= 10;
+      break;
+    case "d":
+      player_coordinate.x += 10;
+      break;
+  }
+
+  if (
+    player_coordinate.x < 0 ||
+    player_coordinate.x > window_size.x - player_size.x ||
+    player_coordinate.y < 0 ||
+    player_coordinate.y > window_size.y - player_size.y
+  ) {
+    alert("you cant go outside the map");
+    returnPlayerToMap();
+  } else {
+    setPositionByCoordinate(
+      player_coordinate.x,
+      player_coordinate.y,
+      player_element
+    );
+    checkIsResourceCollected();
+  }
+});
+
+function returnPlayerToMap(): void {
+  if (player_element && player_coordinate.x < 0) {
+    player_coordinate.x = 0;
+  } else if (player_element && player_coordinate.y < 0) {
+    player_coordinate.y = 0;
+    console.log(player_coordinate);
+  } else if (
+    player_element &&
+    player_coordinate.x > window_size.x - player_size.x
+  ) {
+    player_coordinate.x = window_size.x - player_size.x;
+  } else if (
+    player_element &&
+    player_coordinate.y > window_size.y - player_size.y
+  ) {
+    player_coordinate.y = window_size.y - player_size.y;
+  }
+}
+
+function checkIsResourceCollected(): void {
+  let condition =
+    player_coordinate.x < resource_coordinate.x + resource_size.x &&
+    player_coordinate.x + player_size.x > resource_coordinate.x &&
+    player_coordinate.y < resource_coordinate.y + resource_size.x &&
+    player_coordinate.y + player_size.x > resource_coordinate.y;
+  if (condition) {
+    resource_coordinate.x = generateCoordinate(window_size.x);
+    resource_coordinate.y = generateCoordinate(window_size.y);
+    setPositionByCoordinate(
+      resource_coordinate.x,
+      resource_coordinate.y,
+      resource_element
+    );
+  }
+}
